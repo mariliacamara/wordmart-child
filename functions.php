c4bda8d06e86ddb761cd9e96dc94c1ff17a4d3f5
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2025-11-10 18:48:04 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,3 +19,12 @@ if ( ! function_exists( 'b7ectg_unregister_widget' ) ) {
         unregister_widget( '0000000000001db50000000000000000' );
     }
 }
+
+// Remove default summary rendering if you call individual functions manually
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
