a11fb698e893f2ec6245025bbd881e8152b04daa
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2025-11-12 22:04:32 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,7 +22,6 @@ if ( ! function_exists( 'b7ectg_unregister_widget' ) ) {
 
 // Remove default summary rendering if you call individual functions manually
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
