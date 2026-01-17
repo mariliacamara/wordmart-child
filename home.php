cf6ecebff6114cd1c6224c0235ba6d1a773e9ceb
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-01-17 17:36:24 -0300

    Update home.php

--- home.php
+++ home.php
@@ -25,48 +25,45 @@ get_header();
         <div class="wd-row wd-grid blog-holder blog-loop" style="--wd-col-lg:3;--wd-col-md:3;--wd-col-sm:1;--wd-gap-lg:20px;--wd-gap-sm:10px;">
           <?php while ($query->have_posts()) : $query->the_post(); ?>
 
-            <article <?php post_class('wd-col wd-post blog-post-loop blog-design-default'); ?>>
+          <article id="post-<?php the_ID(); ?>"
+							<?php post_class('wd-post blog-design-masonry blog-post-loop blog-style-bg wd-add-shadow wd-col'); ?>>
 
-              <div class="post-inner">
+							<div class="wd-post-inner article-inner">
 
-                <?php if (has_post_thumbnail()) : ?>
-                  <div class="entry-thumbnail">
-                    <a href="<?php the_permalink(); ?>">
-                      <?php the_post_thumbnail('large'); ?>
-                    </a>
+								<div class="wd-post-thumb entry-header">
+									<div class="wd-post-img post-img-wrapp">
+										<?php the_post_thumbnail('large'); ?>
+									</div>
 
-                    <div class="post-category">
-                      <?php the_category(', '); ?>
-                    </div>
-                  </div>
-                <?php endif; ?>
+									<a class="wd-fill" href="<?php the_permalink(); ?>"></a>
 
-                <div class="entry-content">
+									<div class="wd-post-date wd-style-with-bg">
+										<span class="post-date-day"><?php echo get_the_date('d'); ?></span>
+										<span class="post-date-month"><?php echo get_the_date('M'); ?></span>
+									</div>
+								</div>
 
-                  <div class="entry-meta">
-                    <span class="posted-on"><?php echo get_the_date(); ?></span>
-                  </div>
+								<div class="wd-post-content article-body-container">
 
-                  <h3 class="entry-title">
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                  </h3>
+									<div class="wd-post-cat wd-style-with-bg meta-post-categories">
+										<?php the_category(', '); ?>
+									</div>
 
-                  <div class="entry-excerpt">
-                    <?php the_excerpt(); ?>
-                  </div>
+									<h3 class="wd-post-title wd-entities-title title post-title">
+										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+									</h3>
 
-                  <div class="entry-footer">
-                    <a class="btn btn-style-link btn-color-primary" href="<?php the_permalink(); ?>">
-                      CONTINUAR LENDO
-                    </a>
-                  </div>
+									<div class="wd-post-excerpt entry-content">
+										<?php the_excerpt(); ?>
+									</div>
 
-                </div>
+									<div class="wd-post-read-more wd-style-link read-more-section">
+										<a href="<?php the_permalink(); ?>">Continuar a ler</a>
+									</div>
 
-              </div>
-
-            </article>
-
+								</div>
+							</div>
+						</article>
 
           <?php endwhile; ?>
 
