b85611cde94d7c8c8cd1608836eb3f74917ff521
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-02-26 19:24:05 -0300

    Update content-single-product.php

--- content-single-product.php
+++ content-single-product.php
@@ -525,6 +525,11 @@ global $product;
     input.dispatchEvent(new Event('change', { bubbles: true }));
   }
 
+  // Adiciona os event listeners para os botões +/-
+  ['click', 'pointerdown', 'mousedown', 'touchstart'].forEach(evt => {
+    document.addEventListener(evt, onQtyClick, true);
+  });
+
   const relatedSection = document.querySelector('.related-products');
 
   if (relatedSection) {
