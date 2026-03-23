cf7de00632802958381a1b67108117a1b8202a8a
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-03-23 18:16:33 -0300

    Update autoupdate-cart.js

--- assets/js/autoupdate-cart.js
+++ assets/js/autoupdate-cart.js
@@ -1,16 +1,39 @@
 jQuery(function($) {
-  let timeout;
 
-  $('.woocommerce-cart-form').on('input change', 'input.qty', function(){
-    if (timeout) clearTimeout(timeout);
+  function initAutoUpdate() {
+    let timeout;
 
-    timeout = setTimeout(function() {
-      const $btn = $('[name="update_cart"]');
+    $('.woocommerce-cart-form').off('input.autoUpdate change.autoUpdate');
 
-      if ($btn.length) {
-        $btn.prop('disabled', false);
-        $btn.trigger('click');
+    $('.woocommerce-cart-form').on('input.autoUpdate change.autoUpdate', 'input.qty', function() {
+      let $input = $(this);
+      let value = parseInt($input.val());
+
+      // 🔒 impede 0 ou menor
+      if (!value || value < 1) {
+        $input.val(1);
+        value = 1;
       }
-    }, 500);
+
+      if (timeout) clearTimeout(timeout);
+
+      timeout = setTimeout(function() {
+        const $btn = $('[name="update_cart"]');
+
+        if ($btn.length) {
+          $btn.prop('disabled', false);
+          $btn.trigger('click');
+        }
+      }, 500);
+    });
+  }
+
+  // init inicial
+  initAutoUpdate();
+
+  // 🔁 rebind após update AJAX do WooCommerce
+  $(document.body).on('updated_wc_div updated_cart_totals', function() {
+    initAutoUpdate();
   });
+
 });
