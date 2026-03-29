6b9c58df73e7cfa5df1323d97788f47db6ef32cc
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2026-03-29 20:11:53 -0300

    Update product-cards.js

--- assets/js/product-cards.js
+++ assets/js/product-cards.js
@@ -7,10 +7,14 @@ document.addEventListener("click", function(e) {
     const productId = btn.dataset.productId;
     const card = btn.closest("li.product");
 
-    fetch(`/wishlist/?remove_from_wishlist=${productId}`, {
+    fetch(`/?remove_from_wishlist=${productId}`, {
         credentials: "same-origin"
     }).then(() => {
-        card.style.opacity = "0";
-        setTimeout(() => card.remove(), 300);
+
+        if (card) {
+            card.style.opacity = "0";
+            setTimeout(() => card.remove(), 300);
+        }
+
     });
 });
